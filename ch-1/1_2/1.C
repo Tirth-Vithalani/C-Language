915baa5d875360ef20645139ec27b284d4054a58
Tirth-Vithalani <vithalanitirth30@gmail.com>
2024-06-05 12:54:16 +0530

    05/06/2024 - github

---
+++ ch-1/1_2/1.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{       clrscr();
+	printf("Name\t: Tirth Vithalani\n");
+	printf("Age\t: 18\n");
+	printf("School\t: Mauni International school\n");
+	getch();
+}
